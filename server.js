e3bacbccd583f415fc8d3391acb9ad37c30d2825
Vishal Joshi <vishalrjoshi@outlook.com>
2014-09-26 17:17:31 -0700

    everything minus the apn

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
 var http = require('http');
-var apn = require('querystring');
+var qs = require('querystring');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if(req.method === "POST") {
+         var fullBody="";
+     
+         req.on('data', function(chunk){
+            fullBody += chunk;
+            console.log("Full body " + fullBody);
+        });
+ 
+        req.on('end', function(){
+            var data = qs.parse(fullBody);
+            var weddingId = data.weddingId;
+            var announcement = data.announcement;
+
+            console.log("weddingId " +weddingId);
+            console.log("Announcement " + announcement);
+            //var myDevice = new apn.Device(data.token);
+            // Now we need to store it! Add code to interface with a d below...
+ 
+            res.writeHead(200, {"Content-Type": "text/plain"});
+            res.end("weddingId sent was \n" + weddingId + "announcement sent was " + announcement );
+
+            res.end();
+        });
+    }
   res.end('Hello World\n');
+
 }).listen(port);
 // var http = require('http');    
 // var apn = require('apn');
